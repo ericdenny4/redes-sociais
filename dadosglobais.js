61179358a86feca11a0227e897a129d8208a8178
Eric Denny <103936621+ericdenny4@users.noreply.github.com>
2024-10-22 16:47:44 -0300

    2210

--- dadosglobais.js
+++ dadosglobais.js
@@ -7,13 +7,13 @@ async function visualizarInformacoesGlobais(){
     const paragrafo = document.createElement('p');
     paragrafo.classList.add('graficos-container__texto');
     paragrafo.innerHTML = `Você sabia que o mundo tem 
-    ${dados.total_pessoas_mundo} de pessoas e que aproximadamente 
-    ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas`
+    <span>${dados.total_pessoas_mundo} </span> de pessoas e que aproximadamente 
+    <span>${dados.total_pessoas_conectadas} </span>estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas`
 
-    const container = document.getElementsById('graficos-container');
-    container.appendchild(paragrafo);
+    const container = document.getElementById('graficos-container');
+    container.appendChild(paragrafo);
 
     console.log(paragrafo);
 }
 
-visualisarInformacoesGlobais()
+visualizarInformacoesGlobais()
